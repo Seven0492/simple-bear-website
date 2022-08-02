4714367daeb98860d0298162fde0123abe973452
Seven01 <sethm6348@gmail.com>
2022-08-02 18:52:37 -0400

    adding a comment in feedback.php

--- feedback.php
+++ feedback.php
@@ -1,5 +1,6 @@
 <!-- I copied this off some site, don't have the time to learn PHP while learning CSS -->
 <!-- And no I'm not giving you my email, a redirect is fine -->
+<!-- Github pages only support static websites so this doesn't work, but it should work if it were hosted somewhere I paid for -->
 <?php
 if($_POST["Message"]) {
 mail("aie7srmxg@mozmail.com", "Feedback:",
