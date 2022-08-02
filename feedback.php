3d209aaeddd2024968de71032b9f4107eeb2c728
Seven01 <sethm6348@gmail.com>
2022-08-02 18:27:27 -0400

    Trying to get the email feeback working with dependences like Getform

--- feedback.php
+++ feedback.php
@@ -1,1 +1,8 @@
-<!-- I'll figure out how to code php eventually, just not now, so yes. It doesn't work for now -->
+<!-- I copied this off some site, don't have the time to learn PHP while learning CSS -->
+<!-- And no I'm not giving you my email, a redirect is fine -->
+<?php
+if($_POST["Message"]) {
+mail("aie7srmxg@mozmail.com", "Feedback:",
+$_POST["Insert Your Message"]. "From: no_reply_feedback@simple-bear-website.com");
+}
+?>
